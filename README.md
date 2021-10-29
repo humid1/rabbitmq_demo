7bf0f245b92ab6299046c0fe9ba326514860b66c
qiujianping <qiujianp123@163.com>
2021-10-29 09:38:34 +0800

    perf: 更新README文档

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # RabbitMQ 在 Spring boot中的一些操作
 
-# demo04 jdk1.8 函数式编程
++ demo01 RabbitMQ 简单的操作
++ demo02 使用 RabbitMQ 发送邮件案例
++ demo03 使用 RabbitMQ 延时队列
+
+# jdk1.8 函数式编程
+
++ demo04 webflux的使用
++ demo05 路由函数的使用
++ demo06 使用webflux-client
--- demo02/src/main/java/com/qjp/rabbit/demo02/service/impl/MsgLogServiceImpl.java
+++ demo02/src/main/java/com/qjp/rabbit/demo02/service/impl/MsgLogServiceImpl.java
@@ -1,11 +1,13 @@
 package com.qjp.rabbit.demo02.service.impl;
 
 import com.qjp.rabbit.demo02.service.MsgLogService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author qiujianping
  * @date Created in 2020/9/14 18:29
  */
+@Service
 public class MsgLogServiceImpl implements MsgLogService {
 
 
